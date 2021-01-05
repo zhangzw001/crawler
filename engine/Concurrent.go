2c998154deae7bcd542916809eaf242f8244ad2e
zhangzw <zhangzw@boqii.com>
2021-01-05 17:30:52 +0800

    并发版爬虫架构Scheduler I 添加注释

--- engine/Concurrent.go
+++ engine/Concurrent.go
@@ -23,7 +23,12 @@ func (e ConcurrentEngine) Run(seeds ...Request) {
 		e.Scheduler.Submit(req)
 	}
 
-	//
+	// Scheduler.WorkerChan = in
+	// 因为 in 其实就是 Scheduler.WorkerChan, 所以 worker 会等待读 in chan的数据, in什么时候有数据呢? 答案是 Submit 的时候(s.WorkerChan <- r)
+	// 相当于 scheduler 送给 worker
+	// 因此 worker 等待 in 的输入, 数据来自 scheduler 的 Submit
+	// Submit 的 输入 req 是来自engine的 request
+	// engine 的 request 除了第一次是 seeds 传入, 之后都是来自 worker 返回的 out
 	in := make(chan Request)
 	out := make(chan ParseResult)
 	e.Scheduler.ConfigMasterWorkerChan(in)
